f587644ee46b69f2b2f20b896e95f69e852c804a
JHolmberg <s2000945@edu.bc.fi>
2022-02-09 10:07:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Faker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                var name = Faker.Name.FullName();
+                var address = Faker.Address.StreetAddress();
+                var usPassport = Faker.Identification.UsPassportNumber();
+                var ssn = Faker.Identification.SocialSecurityNumber();
+                var mbi = Faker.Identification.MedicareBeneficiaryIdentifier();
+                Console.WriteLine($"Name: {name}, Address: {address}, Passport: {usPassport}, ssn: {ssn}, mbi: {mbi}");
+                if(i == 3)
+                {
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
